2ff8636199b110f2757f7a46941b9c5e0d3f77a3
Luca Angeletti <lucaangeletti@me.com>
2023-12-18 23:02:10 +0100

    Converted the scene computed property into a constant. In the case of future development for the ContentView, if it had remained a computed property, any updates to the ContentView would have triggered a redraw of the entire view. By making the scene a constant, we prevent the recreation of the entire scene, which is not desirable.

--- Aquatic/ContentView.swift
+++ Aquatic/ContentView.swift
@@ -10,28 +10,28 @@ import SpriteKit
 
 struct ContentView: View {
 
-    private let size: CGSize = .init(width: 600, height: 850)
+    private static let size: CGSize = .init(width: 600, height: 850)
     
-    private var scene: SKScene? {
+    private let scene: SKScene? = {
         do {
-            let gameScene: GameScene = try GameScene(size: size)
+            let gameScene: GameScene = try GameScene(size: Self.size)
             gameScene.scaleMode = .aspectFit
             return gameScene
         } catch {
             debugPrint("Failed to initialize GameScene: \(error)")
             return nil
         }
-    }
+    }()
 
     var body: some View {
         if let scene {
             SpriteView(scene: scene/*, debugOptions: [.showsFPS, .showsNodeCount, .showsPhysics]*/)
                 .ignoresSafeArea()
-                .frame(minWidth: size.width, maxWidth: size.width, minHeight: size.height, maxHeight: size.height)
+                .frame(minWidth: Self.size.width, maxWidth: Self.size.width, minHeight: Self.size.height, maxHeight: Self.size.height)
         } else {
             Text("Error: Failed to initialize game scene")
                 .foregroundColor(.red)
-                .frame(minWidth: size.width, maxWidth: size.width, minHeight: size.height, maxHeight: size.height)
+                .frame(minWidth: Self.size.width, maxWidth: Self.size.width, minHeight: Self.size.height, maxHeight: Self.size.height)
         }
     }
 }
